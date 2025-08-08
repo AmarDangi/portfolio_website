3ff678e175e2fdbd95a662215631906af600abca
Amar Dangi <amardangi046@gmail.com>
2025-08-08 13:07:35 +0530

    down grade tilwand css gem version

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
-//= link application.css
-//= link application.js
-//= link_tree ../builds
+//= link_directory ../stylesheets .css
+//= link active_admin.css
+//= link active_admin.js
+
