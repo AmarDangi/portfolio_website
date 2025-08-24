f49e29a7add8332160d1bd441ffc1732a75e63d5
Amar Dangi <amardangi046@gmail.com>
2025-08-24 21:10:40 +0530

    add github or linkdin links

--- app/admin/abouts.rb
+++ app/admin/abouts.rb
@@ -1,28 +1,47 @@
 ActiveAdmin.register About do
-    permit_params :name, :bio, :photo, :resume
-  
-    form do |f|
-      f.inputs do
-        f.input :name
-        f.input :bio
-        f.input :photo, as: :file
-        f.input :resume, as: :file
+  permit_params :name, :bio, :photo, :resume
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :bio
+      f.input :photo, as: :file
+      f.input :resume, as: :file
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :bio
+    column :photo do |about|
+      if about.photo.attached?
+        image_tag url_for(about.photo), size: "80x80"
       end
-      f.actions
+    end
+    column :created_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :bio
+      row :photo do |about|
+        if about.photo.attached?
+          image_tag url_for(about.photo), size: "150x150"
+        end
+      end
+      row :resume do |about|
+        if about.resume.attached?
+          link_to about.resume.filename, url_for(about.resume), target: "_blank"
+        end
+      end
+      row :created_at
+      row :updated_at
     end
   end
-  
-  # # app/admin/projects.rb
-  # ActiveAdmin.register Project do
-  #   permit_params :title, :description, :link, :image
-  
-  #   form do |f|
-  #     f.inputs do
-  #       f.input :title
-  #       f.input :description
-  #       f.input :link
-  #       f.input :image, as: :file
-  #     end
-  #     f.actions
-  #   end
-  # end
+end
